ffe2b9b3cafd0c49936e997a295fffaabf279318
SrGiovanni <samuelefulghi@gmail.com>
2022-06-07 11:38:13 -0400

    add mutations and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,16 +28,31 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     thoughts(username: String): [Thought]
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
 `;
 
 // export the typeDefs
