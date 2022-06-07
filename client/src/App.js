c9479074a7ef8e9fbf87141958ec356e7a00dea5
SrGiovanni <samuelefulghi@gmail.com>
2022-06-07 18:56:41 -0400

    fix proxy and add screenshot

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
