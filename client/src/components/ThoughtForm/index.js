07984fa6ffa51fc83648222d750d708bff636ae0
SrGiovanni <samuelefulghi@gmail.com>
2022-06-07 18:52:15 -0400

    add reaction capability

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
   const [thoughtText, setText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+  
+        // could potentially not exist yet, so wrap in a try/catch
+      try {
+        // update me array's cache
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+        });
+      } catch (e) {
+        console.warn("First thought insertion by user!")
+      }
+  
+      // update thought array's cache
+      const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+      cache.writeQuery({
+        query: QUERY_THOUGHTS,
+        data: { thoughts: [addThought, ...thoughts] },
+      });
+    }
+  });
 
   const handleChange = event => {
     if (event.target.value.length <= 280) {
@@ -13,14 +40,26 @@ const ThoughtForm = () => {
 
   const handleFormSubmit = async event => {
     event.preventDefault();
-    setText('');
-    setCharacterCount(0);
+  
+    try {
+      // add thought to database
+      await addThought({
+        variables: { thoughtText }
+      });
+  
+      // clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import Auth from '../utils/auth';
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
@@ -36,6 +38,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
